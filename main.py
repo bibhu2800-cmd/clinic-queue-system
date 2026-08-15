478e7055712646f2c0ee506eafb96d495dcea285
Macbook <macbook@Bibhus-MacBook-Air.local>
2026-08-15 12:20:28 +0530

    Fix websocket serialization and text broadcast

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class ConnectionManager:
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
-                await connection.send_json(message)
+                await connection.send_text(message)
 
 manager = ConnectionManager()
 
