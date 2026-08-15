4b3915a9b72384f825306c8d150a91786f5b671e
Macbook <macbook@Bibhus-MacBook-Air.local>
2026-08-15 11:57:44 +0530

    Fix static files mount path

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.mount("/static", StaticFiles(directory="." ,html=True), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 async def root():
     return RedirectResponse(url="/static/index.html")
